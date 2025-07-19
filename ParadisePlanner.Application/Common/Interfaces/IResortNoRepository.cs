56f052bc261f964fd093ea7ef9876f86c6bc1c17
Sehar Ajmal <seharajmal452@gmail.com>
2025-07-19 10:59:46 +0500

    Application

---
+++ ParadisePlanner.Application/Common/Interfaces/IResortNoRepository.cs
@@ -0,0 +1,14 @@
+﻿using ParadisePlanner.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParadisePlanner.Application.Common.Interfaces
+{
+    public interface IResortNoRepository : IRepository<ResortNo>
+    {
+        void Update(ResortNo entity);
+    }
+}
---
+++ ParadisePlanner.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParadisePlanner.Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IResortRepository Resort { get; }
+        IResortNoRepository ResortNo { get; }
+        IAmenityRepository Amenity { get; }
+        void Save();
+    }
+}
